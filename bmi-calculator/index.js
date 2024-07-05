f309ef2ab992aad77f38055ac944bbb60198a1b8
mauro258 <mejiamauricio1009@gmail.com>
2024-07-05 16:31:30 -0500

    first commit

---
+++ bmi-calculator/index.js
@@ -0,0 +1,47 @@
+import readline from 'readline'
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+  
+  // Función para calcular el BMI
+  function calculateBMI(weight, height) {
+    return weight / (height ** 2);
+  }
+  
+  // Función para interpretar el BMI
+  function interpretBMI(bmi) {
+    if (bmi < 18.5) {
+      return "Bajo peso";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+      return "Normal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+      return "Sobrepeso";
+    } else {
+      return "Obesidad";
+    }
+  }
+  
+  // Solicitar entrada del usuario para el peso
+  rl.question('Introduce tu peso en kilogramos: ', (weightInput) => {
+    const weight = parseFloat(weightInput);
+  
+    // Solicitar entrada del usuario para la altura
+    rl.question('Introduce tu altura en metros: ', (heightInput) => {
+      const height = parseFloat(heightInput);
+  
+      // Calcular el BMI
+      const bmi = calculateBMI(weight, height);
+  
+      // Imprimir el BMI
+      console.log(`Tu BMI es: ${bmi.toFixed(2)}`);
+  
+      // Interpretar y imprimir el resultado
+      const interpretation = interpretBMI(bmi);
+      console.log(`Interpretación: ${interpretation}`);
+  
+      // Cerrar la interfaz de readline
+      rl.close();
+    });
+  });
